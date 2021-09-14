14e0310d404552455b5e3613d244423258ea4d0b
luizbarueri <luizbarueri@gmail.com>
2021-09-14 11:57:59 -0300

    atualização projeto cadastro de eventos

--- cadastroEventos.js
+++ cadastroEventos.js
@@ -1,16 +1,36 @@
-var palestrantes = [];
-var participantes = [];
+var palestrantes = ["Maria","José"];
+var participantes = [ "Luiz","Fernanda","Marta","Pedro"];
+var nome;
 var idade = 0;
 var dataAtual = new Date();
-var dataEvento = new Date();
-
-if (dataEvento >= dataAtual) {
-    if (idade >= 18) {
-        console.log("Cadastro permitido");
+var dataEvento;
+//Entrada dos dados do participante
+nome = participantes[1];
+idade = 18;
+//digite a data no formato: mm/dd/yyyy
+dataEvento = new Date("09/11/2021");
+if (participantes.length < 100) {
+    if (dataEvento >= dataAtual) {
+        if (idade >= 18) {
+            console.log("Cadastro Permitido");
+            console.log("---------------------------------");
+            console.log("Palestrante.: " + palestrantes[0]);
+            console.log("Nome........: " + nome);
+            console.log("Idade.......:" + idade);
+            console.log("Data Atual..: " + dataAtual.toDateString());
+            console.log("Data Evento.: " + dataEvento.toDateString());
+            console.log("---------------------------------");
+        } else {
+            console.log("Idade Inválida! (mínima 18 anos)");
+        } 
     } else {
-        console.log("Cadastro não Permitido!");
-    } 
-} else {
-    console.log("Data do Evento Iválido!");
-}
+        console.log("Data do Evento Inválido!");
+        console.log("Data Atual..: " + dataAtual.toDateString());
+        console.log("Data Evento.: " + dataEvento.toDateString());
+    }
+}    
+else {
+    console.log("Cadasto não permitido! Máximo de participantes é igual a 100" );
+}    
+
 
